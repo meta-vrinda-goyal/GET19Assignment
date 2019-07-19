b4516eb3360b1acfb73fad801eef7f81afb86a91
meta-vrinda-goyal <52688940+meta-vrinda-goyal@users.noreply.github.com>
2019-07-19 17:32:31 +0530

    Update NQueen.java

--- Assignment5/src/NQueen.java
+++ Assignment5/src/NQueen.java
@@ -18,24 +18,26 @@ public class NQueen {
 	 * @param row : current row of matrix at which we are looking to place a queen
 	 * @param col : current column of matrix at which we are looking to place a queen
 	 * @param dimensionOfMatrix
-	 * @return boolean : true if it is valid else false
+	 * @return boolean : true if it is valid to place queen else false
 	 */
 	boolean  isValidToPlaceQueen(int board[][], int row, int col,int dimensionOfMatrix){ 
 	     int i, j; 
+	     // If queen exist in upper row 
 	     for (i = row-1; i >= row; i--){ 
-	         if (board[i][col] == 1){ 
-	             return false;   // If queen exist in upper row 
+	         if (board[i][col] == 1){    
+	             return false;
 	         }
 	     }
+	     //if queen exist in upper left diagonal
 	     for (i = row, j = col; i >= 0 && j >= 0; i--, j--){ 
 	         if (board[i][j] == 1){ 
-	             return false;   //if queen exist in upper left diagonal
+	             return false;   
 	         }
 	     }
-	        
+	     //if queen exist in upper right diagonal   
 	     for (i = row, j = col; i >= 0 && j < dimensionOfMatrix; i--, j++){ 
 	    	 if (board[i][j] == 1){ 
-	    		 return false;    //if queen exist in upper right diagonal
+	    		 return false;    
 	    	 }
 	     }
 	     return true; // if it is valid to place a queen
@@ -53,10 +55,10 @@ public class NQueen {
 			return true;
 		}
 		for (int i = 0; i < dimensionOfMatrix; i++){ 
-			/* check if valid then place queen */
+		// check if valid then place queen 
 	        if (isValidToPlaceQueen(board,startRow,i,dimensionOfMatrix)){ 
 	            board[startRow][i] = 1; 
-	            	/*RECURSIVE CALL with next row as starting row*/
+	            	//RECURSIVE CALL with next row as starting row
 	                if (nQueen(board, startRow + 1, dimensionOfMatrix)){
 	                	return true; 
 	                }
