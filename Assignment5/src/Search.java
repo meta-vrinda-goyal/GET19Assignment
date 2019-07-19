c342215d8ab5d43dbe539ed0001978106b16aaf1
meta-vrinda-goyal <52688940+meta-vrinda-goyal@users.noreply.github.com>
2019-07-19 13:38:50 +0530

    Update Search.java

--- Assignment5/src/Search.java
+++ Assignment5/src/Search.java
@@ -7,22 +7,22 @@ public class Search {
 	 * @return index if element is found else throw assertion error
 	 */
 	public static int doLinearSearch(int arr[],int num){
-		if(arr.length>=1){
-			if(arr[0] == num ){
-				return 0;
-			}
-			//form duplicate array beginning with second element of previous array
-			else{
-				int[] newArr = new int[arr.length-1]; //new array
-				for(int i = 1; i < arr.length; i++){
-					newArr[i-1] = arr [i]; 
-				}
-				return 1+doLinearSearch(newArr, num);//recursion call on new array
-			}
+	    if(arr.length>=1){
+	        if(arr[0] == num ){
+		    return 0;
 		}
+		//form duplicate array beginning with second element of previous array
 		else{
-			throw new AssertionError();//throw if element not found
+		    int[] newArr = new int[arr.length-1]; //new array
+		    for(int i = 1; i < arr.length; i++){
+			newArr[i-1] = arr [i]; 
+		    }
+		    return 1+doLinearSearch(newArr, num);//recursion call on new array
 		}
+	    }
+	    else{
+		throw new AssertionError();//throw if element not found
+	    }
 	}
 	
 	/**
@@ -31,34 +31,32 @@ public class Search {
 	 * @return index at which element occurs else throw assertion error
 	 */
 	public static int doBinarySearch(int arr[],int num) {
-		if (arr.length>=1) {
-			int mid = arr.length/2; // middle element
-			if(arr[mid] == num ) {
-				return mid;
-			}
-			else {
-				if(arr[mid] < num) { //if middle element is lesser then search second half of array
-					
-					int[] newArr = new int[arr.length-mid-1];
-					for (int i = mid+1,j=0; i < arr.length; i++ ) {
-						newArr[j] = arr[i];
-						j++;
-					}
-					return mid + doBinarySearch(newArr, num) +1;
+	    if (arr.length>=1) {
+	        int mid = arr.length/2; // middle element
+		    if(arr[mid] == num ) {
+			return mid;
+		    }
+		    else {
+			if(arr[mid] < num) { //if middle element is lesser then search second half of array
+			    int[] newArr = new int[arr.length-mid-1];
+				for (int i = mid+1,j=0; i < arr.length; i++ ) {
+				    newArr[j] = arr[i];
+				    j++;
 				}
-				else{  //if if middle element is larger then search first half of array
-					int[] newArr = new int[mid];
-					for (int i = 0; i < mid; i++ ) {
-						newArr[i] = arr[i];
-					}
-					return doBinarySearch(newArr, num);
-				}
+				return mid + doBinarySearch(newArr, num) +1;
 			}
-		}
-		else {
-			throw new AssertionError();
-		}
-	
+			else{  //if if middle element is larger then search first half of array
+			    int[] newArr = new int[mid];
+				for (int i = 0; i < mid; i++ ) {
+				    newArr[i] = arr[i];
+				}
+				return doBinarySearch(newArr, num);
+			}
+		    }
+	    }
+	    else {
+		throw new AssertionError();
+	    }
 	}
 }
 
