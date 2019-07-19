05fd0861f3b3e23a5a43b2102291d2c80eb237c8
meta-vrinda-goyal <52688940+meta-vrinda-goyal@users.noreply.github.com>
2019-07-19 13:33:48 +0530

    Update LcmHcf.java

--- Assignment5/src/LcmHcf.java
+++ Assignment5/src/LcmHcf.java
@@ -3,7 +3,7 @@
  * @author vrinda
  *
  */
-public class LcmHcf {
+public class LcmHcf{
 	/**
 	 * uses recursion to solve LCM of two numbers
 	 * @param num1 first integer
@@ -11,45 +11,45 @@ public class LcmHcf {
 	 * @return LCM of first and second integer
 	 */
 	public static int getLcm(int num1,int num2) {
-		if (num1 == num2) {
-			return num1;
+	    if (num1 == num2) {
+	        return num1;
+            }
+	    int smallNum = num1 < num2 ? num1 : num2;
+	    int bigNum = num1 < num2 ? num2 :num1;
+	    //base case
+	    if (smallNum == 1) {
+	        return bigNum;
+	    }
+	    int fact; //store the factors
+	    for ( fact = 2; fact <= bigNum; fact++) {
+	        if (smallNum%fact != 0 && bigNum%fact !=0 ) { // if both the numbers are not divisible check next factor
+		    continue;
 		}
-		int smallNum = num1 < num2 ? num1 : num2;
-		int bigNum = num1 < num2 ? num2 :num1;
-		//base case
-		if (smallNum == 1) {
-			return bigNum;
+		if (smallNum%fact == 0) {
+		    smallNum /= fact;
+			if (bigNum%fact ==0) {
+			    bigNum /= fact;
+		  	        break;	
+			    }
+			else {
+			    break;
+			}
 		}
-		int fact; //store the factors
-		for ( fact = 2; fact <= bigNum; fact++) {
-			if (smallNum%fact != 0 && bigNum%fact !=0 ) { // if both the numbers are not divisible check next factor
-				continue;
-			}
-			if (smallNum%fact == 0) {
-				smallNum /= fact;
-				if (bigNum%fact ==0) {
-					bigNum /= fact;
-				    break;	
-				}
-				else {
-					break;
-				}
-			}
 			
-			if (bigNum%fact == 0) {
-				bigNum /= fact;
-				if (smallNum%fact ==0) {
-					smallNum /= fact;
-				    break;	
-				}
-				else {
-				    break;	
-				}
-			}
+		if (bigNum%fact == 0) {
+		    bigNum /= fact;
+		    if (smallNum%fact ==0) {
+		        smallNum /= fact;
+		        break;	
+		    }
+	            else {
+		        break;	
+		    }
 		}
+	    }
 		// recursion call 
 		return fact*getLcm(smallNum,bigNum);
-	}
+        }
 	/**
 	 * uses recursion to get HCF of two numbers (Euclidean theorem)
 	 * @param num1 first integer
@@ -57,10 +57,9 @@ public class LcmHcf {
 	 * @return HCF of first and second integer
 	 */
 	public static int getHcf(int num1, int num2) {
-		if(num2==0) {
-			return num1;
-		}
-		return getHcf(num2,num1%num2);
-
+	    if(num2==0) {
+	        return num1;
+	    }
+	    return getHcf(num2,num1%num2);
 	}
 }
