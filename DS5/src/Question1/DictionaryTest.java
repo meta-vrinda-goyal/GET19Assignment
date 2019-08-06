9ffc9bb6a8d1dfe893174207d8694af6765e3db1
meta-vrinda-goyal <52688940+meta-vrinda-goyal@users.noreply.github.com>
2019-08-06 10:49:57 +0530

    Update DictionaryTest.java

--- DS5/src/Question1/DictionaryTest.java
+++ DS5/src/Question1/DictionaryTest.java
@@ -17,6 +17,7 @@ public class DictionaryTest {
 		List<DataEntry> list = new ArrayList<DataEntry>();
 		d.addData(33, "hu");
 		d.addData(33, "hu");
+		
 		list = d.getSortedList();
 		for(int i = 0; i<list.size();i++) {
 			System.out.println(list.get(i).getKey() + " " + list.get(i).getValue());
@@ -32,5 +33,14 @@ public class DictionaryTest {
 		Dictionary d = new Dictionary("file.json");
 		assertEquals("key not found",d.getValue(277));
 	}
-	
+	@Test
+	public void removeDataTest() {
+		Dictionary d = new Dictionary("file.json");
+		List<DataEntry> list = new ArrayList<DataEntry>();
+		d.removeData(4);
+		list = d.getSortedList();
+		for(int i = 0; i<list.size();i++) {
+			System.out.println(list.get(i).getKey() + " " + list.get(i).getValue());
+		}
+	}
 }
