7286982aee0c07adbdf91370ad907ea725be419f
meta-vrinda-goyal <52688940+meta-vrinda-goyal@users.noreply.github.com>
2019-08-06 11:26:43 +0530

    Update EmployeeLinkedListTest.java

--- DS5/src/Question2/EmployeeLinkedListTest.java
+++ DS5/src/Question2/EmployeeLinkedListTest.java
@@ -1,17 +1,20 @@
 package Question2;
-
+import static org.junit.Assert.*;
+import java.util.ArrayList;
 import org.junit.Test;
 
 public class EmployeeLinkedListTest {
 
 	@Test
 	public void LinkedListSortingTest() {
-		EmployeeLinkedList list = new EmployeeLinkedList();
+	    EmployeeLinkedList list = new EmployeeLinkedList();
 	    list.insertEmployee(new Employee("abc", 12, 13));
 	    list.insertEmployee(new Employee("def", 33, 13));
 	    list.insertEmployee(new Employee("vfrf", 25, 50));
 	    InsertionSort sort = new InsertionSort();
-	    sort.sort(list,list.size);
-	    list.print();
+	    String arr[] = new String[]{"vrinda","abc","def"};
+	    list = sort.sort(list,list.size);
+	    String arr2 [] = list.print();
+	    assertArrayEquals(arr,arr2);
 	}
 }
