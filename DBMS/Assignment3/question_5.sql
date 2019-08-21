dae604337e2b85fdf905ef67125fdaff11201e8d
meta-vrinda-goyal <52688940+meta-vrinda-goyal@users.noreply.github.com>
2019-08-21 18:38:24 +0530

    Update question_5.sql

--- DBMS/Assignment3/question_5.sql
+++ DBMS/Assignment3/question_5.sql
@@ -40,34 +40,3 @@ FROM
 GROUP BY product_id
 ORDER BY quan DESC;
 
-#Select and display the category title of all top parent categories sorted alphabetically and the count of their child categories.
-SELECT DISTINCT
-    c.name, COUNT(p.category_id)
-FROM
-    category c
-        LEFT JOIN
-    category p ON c.category_id = p.parent_category
-WHERE
-    c.parent_category IS NULL
-GROUP BY p.parent_category
-ORDER BY c.name;
-
-/*Given the id of a user, fetch all orders (Id, Order Date, Order Total) of that user which are in shipped state.
-Orders should be sorted by order date column in chronological order*/
-SELECT 
-    o.order_id, o.date, c.sum
-FROM
-    cart c,
-    orders o
-WHERE
-    c.shopper_id = 2 AND o.shopper_id = c.shopper_id AND o.cart_id = c.cart_id AND order_status = 'Shipped'
-ORDER BY date;
-
-/*Delete all those products which were not ordered by any Shopper in last 1 year. */
-DELETE FROM product WHERE product_id  NOT IN (SELECT DISTINCT product_id 
-FROM cart c, orders o WHERE o.cart_id = c.cart_id AND date > CURRENT_DATE - 365 );
-
-SELECT 
-    *
-FROM
-    PRODUCT;
