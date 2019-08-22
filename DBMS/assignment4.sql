d7eb9571b6b34c4bfc48fa998770cfdf3068d8ba
meta-vrinda-goyal <52688940+meta-vrinda-goyal@users.noreply.github.com>
2019-08-22 11:31:47 +0530

    Update assignment4.sql

--- DBMS/assignment4.sql
+++ DBMS/assignment4.sql
@@ -47,4 +47,12 @@ DELIMITER ;
 CALL getOrderDetail('2019-08-01', '2019-08-30');
 CALL getOrderDetail('2019-08-30','2019-08-13');
 
+/*Identify the columns require indexing in order, product, category tables and create indexes.*/
+ALTER TABLE orders ADD INDEX index_date_of_order(date);
+ALTER TABLE orders ADD INDEX index_shopper_id(shopper_id);
+ALTER TABLE product ADD INDEX index_name(name);
+ALTER TABLE categories ADD INDEX index_parent_id(parent_category);
+ALTER TABLE categorylink ADD INDEX index_category_id(category_id);
+ALTER TABLE categorylink ADD INDEX index_product_id(product_id);
+ALTER TABLE items_ordered ADD INDEX index_order_status(order_status);
 
