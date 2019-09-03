5ce814ab848be295f3c78c6a2f8df7a8367f8fd4
meta-vrinda-goyal <52688940+meta-vrinda-goyal@users.noreply.github.com>
2019-09-03 16:59:38 +0530

    Add files via upload

---
+++ EAD/EAD-Session7-SpringIntro/EadSession7Application.java
@@ -0,0 +1,14 @@
+
+package com.metacube.EADSession7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadSession7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadSession7Application.class, args);
+	}
+
+}
