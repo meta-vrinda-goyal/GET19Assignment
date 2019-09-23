a244a6a7610cb8fd29eb98acfdf0dabe748f25ef
meta-vrinda-goyal <52688940+meta-vrinda-goyal@users.noreply.github.com>
2019-09-23 10:25:58 +0530

    Add files via upload

---
+++ EAD/EADSession13/src/main/java/com/metacube/assignment/EADSession13/EadSession13Application.java
@@ -0,0 +1,13 @@
+package com.metacube.assignment.EADSession13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadSession13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadSession13Application.class, args);
+	}
+
+}
